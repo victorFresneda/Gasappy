54efd61f5f519f7739d8b3f291247ccd7afa0404
victorFresneda <vafresnedah@hotmail.com>
2020-06-22 03:56:37 -0500

    Se añade activity select method, con los componentes  EditText-TextView

--- app/src/main/java/com/example/gasappy/MainActivity.kt
+++ app/src/main/java/com/example/gasappy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gasappy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,13 +11,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        click_buttton_cal()
+        click_buttton_void()
+        click_buttton_cal ()
     }
 
       fun click_buttton_cal (){
                  butCal.setOnClickListener(){
-                     Toast.makeText(applicationContext, " boton funciona ", Toast.LENGTH_SHORT).show()
+                     val calculationWindow: Intent = Intent(applicationContext, SelectMethod::class.java)
+                     startActivity(calculationWindow)
                  }
 
       }
+
+    fun click_buttton_void (){
+        butVoid.setOnClickListener(){
+            Toast.makeText(applicationContext, " en comstruccion ", Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/gasappy/SelectMethod.kt
@@ -0,0 +1,12 @@
+package com.example.gasappy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectMethod : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_method)
+    }
+}
