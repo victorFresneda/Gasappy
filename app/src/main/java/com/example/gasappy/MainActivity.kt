0074b6c297e04eb6a634a8cdd1a4ecf6fa3e1617
victorFresneda <vafresnedah@hotmail.com>
2020-06-21 23:03:59 -0500

    Se realiza animacion de botones menu principal

--- app/src/main/java/com/example/gasappy/MainActivity.kt
+++ app/src/main/java/com/example/gasappy/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.gasappy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        click_buttton_cal()
     }
+
+      fun click_buttton_cal (){
+                 butCal.setOnClickListener(){
+                     Toast.makeText(applicationContext, " boton funciona ", Toast.LENGTH_SHORT).show()
+                 }
+
+      }
 }
